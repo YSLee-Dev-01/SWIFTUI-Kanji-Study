8333a162eb83a159af74d8bfa9316eb5bd846180
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-18 10:19:08 +0900

    first commit

---
+++ SWIFTUI_KanjiStudy/SWIFTUI_KanjiStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_KanjiStudyApp.swift
+//  SWIFTUI_KanjiStudy
+//
+//  Created by 이윤수 on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_KanjiStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
