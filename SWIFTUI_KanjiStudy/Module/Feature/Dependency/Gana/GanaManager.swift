3be6300a15d5c24cc1a2aa55f75e31cd60e24e7b
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-19 15:16:57 +0900

    Feat(Gana): Gana를 가져올 수 있는 Dependency 파일 추가 및 경로 지정

---
+++ SWIFTUI_KanjiStudy/Module/Feature/Dependency/Gana/GanaManager.swift
@@ -0,0 +1,12 @@
+//
+//  GanaManager.swift
+//  SWIFTUI_KanjiStudy
+//
+//  Created by 이윤수 on 8/19/25.
+//
+
+import Foundation
+
+struct GanaManager {
+   
+}
---
+++ SWIFTUI_KanjiStudy/Module/Feature/Dependency/Gana/GanaManagerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  GanaManagerProtocol.swift
+//  SWIFTUI_KanjiStudy
+//
+//  Created by 이윤수 on 8/19/25.
+//
+
+import Foundation
