1caff373d6540d594e3cac7cfcc65d1e52a60c1e
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-20 09:30:52 +0900

    Fix(CommonView): MainStyleView를 구성하는 방식 변경 (cornerRadius을 자유롭게 설정할 수 있도록)

--- SWIFTUI_KanjiStudy/Module/Component/CommonView/MainStyleView.swift
+++ SWIFTUI_KanjiStudy/Module/Component/CommonView/MainStyleView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct MainStyleView<Contents>: View where Contents: View {
+    let cornerRadius: CGFloat
     @ViewBuilder var contents: Contents
+    
+    init(cornerRadius: CGFloat = 15,  @ViewBuilder contents: @escaping () -> Contents) {
+        self.cornerRadius = cornerRadius
+        self.contents = contents()
+    }
     
     var body: some View {
         self.contents
             .background {
-                RoundedRectangle(cornerRadius: 15)
+                RoundedRectangle(cornerRadius: self.cornerRadius)
                     .fill(Color.gray.opacity(0.1))
             }
     }
