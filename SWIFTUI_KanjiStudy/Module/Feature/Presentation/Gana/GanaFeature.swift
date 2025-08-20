42692457e81d631fb1503d27177cc6b431119cdb
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-20 09:37:24 +0900

    Feat(Gana): 기본 디자인 구성

--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/Gana/GanaFeature.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/Gana/GanaFeature.swift
@@ -10,18 +10,32 @@ import ComposableArchitecture
 
 @Reducer
 struct GanaFeature: Reducer {
+    @Dependency(\.ganaManager) var ganaManager
+    
     @ObservableState
     struct State: Equatable {
+        let isHiragana: Bool
+        var gana: [[String]] = []
         
+        init(isHiragana: Bool) {
+            self.isHiragana = isHiragana
+        }
     }
     
     enum Action: Equatable {
-        
+        case onAppear
+        case backBtnTapped
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                if state.gana.isEmpty {
+                    state.gana = state.isHiragana ?  ganaManager.getGiragana() : ganaManager.getKatakana()
+                }
+                return .none
+                
             default: return .none
             }
         }
--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/Gana/GanaView.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/Gana/GanaView.swift
@@ -16,10 +16,60 @@ struct GanaView: View {
     }
     
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            LargeNavigationBar(title: self.store.isHiragana ? "히라가나" : "가타카나"){
+                self.store.send(.backBtnTapped)
+            }
+        
+            MainStyleView(cornerRadius: 0) {
+                ExpandedView(alignment: .center) {
+                    HStack(spacing: 10) {
+                        Image(systemName: "info.circle")
+                            .resizable()
+                            .frame(width: 17, height: 17)
+                            .foregroundStyle(Color.black.opacity(0.6))
+                        
+                        Text("가나를 눌러서 상세정보를 확인해보세요.")
+                            .foregroundStyle(Color.black.opacity(0.6))
+                            .font(.system(size: 16, weight: .medium))
+                    }
+                }
+                .frame(height: 40)
+            }
+            
+            VStack(spacing: 15) {
+                ForEach(Array(self.store.gana.enumerated()), id: \.offset) { _, row in
+                    HStack(spacing: 15) {
+                        ForEach(Array(row.enumerated()), id: \.offset) { _, item in
+                            if !item.isEmpty {
+                                Button {
+                                    
+                                } label : {
+                                    MainStyleView {
+                                        ExpandedView(alignment: .center) {
+                                            Text(item)
+                                                .font(.system(size: 17, weight: .light))
+                                                .foregroundStyle(Color.black)
+                                                .frame(maxHeight: 55)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, 20)
+            .frame(maxHeight: .infinity)
+            
+        }
+        .padding(.vertical, 20)
+        .onAppear {
+            self.store.send(.onAppear)
+        }
     }
 }
 
 #Preview {
-    GanaView(store: .init(initialState: .init(), reducer: {GanaFeature()}))
+    GanaView(store: .init(initialState: .init(isHiragana: true), reducer: {GanaFeature()}))
 }
