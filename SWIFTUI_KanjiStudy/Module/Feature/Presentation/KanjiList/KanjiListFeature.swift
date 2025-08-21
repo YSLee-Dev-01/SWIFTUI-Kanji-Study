e0e5e889252e7c9ecafb63e86c6d00eea9e55bc8
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-21 16:15:31 +0900

    Feat(KanjiList): 기본 디자인 구성

--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiList/KanjiListFeature.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiList/KanjiListFeature.swift
@@ -8,19 +8,34 @@
 import Foundation
 import ComposableArchitecture
 
+struct Step: Equatable {
+    let title: String
+    let count: Int
+}
+
 @Reducer
 struct KanjiListFeature: Reducer {
+    @Dependency(\.kanjiManager) var kanjiManager
+    
     @ObservableState
     struct State: Equatable {
-        
+        var steps: [Step] = []
     }
     
     enum Action: Equatable {
+        case onAppear
+        case backBtnTapped
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                if state.steps.isEmpty {
+                    state.steps = kanjiManager.groupKanjiByJLPTLevel().map {Step(title: $0.key, count: $0.value.count)}.sorted {$0.title > $1.title}
+                }
+                return .none
+                
             default: return .none
             }
         }
