0998682bdc9cd24bf2120b26b209d9f9a44f231e
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-21 14:08:56 +0900

    Feat(Dependency(Kanji)): 한자 매니저 파일 추가

---
+++ SWIFTUI_KanjiStudy/Module/Feature/Dependency/Kanji/KanjiManagerKey.swift
@@ -0,0 +1,8 @@
+//
+//  KanjiManagerKey.swift
+//  SWIFTUI_KanjiStudy
+//
+//  Created by 이윤수 on 8/21/25.
+//
+
+import Foundation
---
+++ SWIFTUI_KanjiStudy/Module/Feature/Dependency/Kanji/KanjiManagerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  KanjiManagerProtocol.swift
+//  SWIFTUI_KanjiStudy
+//
+//  Created by 이윤수 on 8/21/25.
+//
+
+import Foundation
