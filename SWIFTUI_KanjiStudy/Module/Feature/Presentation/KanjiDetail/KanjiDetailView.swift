98cb01640ac464368ba08d8d31f977dd59e2bf47
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-25 11:29:12 +0900

    Feat(KanjiDetail): 기본 디자인 구성

--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiDetail/KanjiDetailView.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiDetail/KanjiDetailView.swift
@@ -16,10 +16,59 @@ struct KanjiDetailView: View {
     }
     
     var body: some View {
-        Text("KanjiDetailView")
+        VStack(spacing: 20) {
+            StandardNavigationScrollView(title: "JLPT \(self.store.selectedJLPTLevel)", backBtnConfigAction: {
+                self.store.send(.backBtnTapped)
+            }) {
+                
+                LazyVStack(spacing: 20) {
+                    ForEach(Array(self.store.kanjiList.enumerated()), id: \.offset) { row, data in
+                        MainStyleView {
+                            Button {
+                                
+                            } label: {
+                                ExpandedView(alignment: .center) {
+                                    Text(data.kanji)
+                                        .foregroundStyle(Color.black)
+                                        .font(.system(size: 30, weight: .medium))
+                                        .padding(30)
+                                }
+                                .overlay {
+                                    VStack {
+                                        ExpandedView(alignment: .leading) {
+                                            Text("\(row + 1)번째")
+                                                .foregroundStyle(Color.black.opacity(0.4))
+                                                .font(.system(size: 15, weight: .light))
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        HStack {
+                                            Image(systemName: "star")
+                                                .resizable()
+                                                .frame(width: 17, height: 17)
+                                                .foregroundStyle(Color.black.opacity(0.4))
+                                            
+                                            Spacer()
+                                            
+                                            Image(systemName: "chevron.down")
+                                                .resizable()
+                                                .frame(width: 15, height: 8)
+                                                .foregroundStyle(Color.black.opacity(0.4))
+                                        }
+                                    }
+                                }
+                                .padding(20)
+                            }
+                        }
+                    }
+                }
+                .padding(.vertical, 20)
+            }
+        }
     }
 }
 
 #Preview {
-    KanjiDetailView(store: .init(initialState: .init(kanjiList: [KanjiInfo(id: 1, kanji: "", strokes: nil, jlptLevel: .N1, description: "")], selectedJLPTLevel: ""), reducer: {KanjiDetailFeature()}))
+    KanjiDetailView(store: .init(initialState: .init(kanjiList: [KanjiInfo(id: 1, kanji: "漢字", strokes: nil, jlptLevel: .N5, description: "설명설명")], selectedJLPTLevel: "N5"), reducer: {KanjiDetailFeature()}))
 }
