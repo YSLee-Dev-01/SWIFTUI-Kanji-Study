80037b7f847c562509129d120ab773f50e9f0dc7
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-27 09:13:20 +0900

    Fix(KanjiDetail): View 구성 방식 변경

--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiDetail/KanjiDetailView.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/KanjiDetail/KanjiDetailView.swift
@@ -16,79 +16,77 @@ struct KanjiDetailView: View {
     }
     
     var body: some View {
-        VStack(spacing: 20) {
-            ScrollViewReader { proxy in
-                StandardNavigationScrollView(title: "JLPT \(self.store.jlptLevel)", backBtnConfigAction: {
-                    self.store.send(.backBtnTapped)
-                }) {
-                    LazyVStack(spacing: 20) {
-                        ForEach(Array(self.store.kanjiList.enumerated()), id: \.offset) { row, data in
-                            MainStyleView {
-                                Button {
-                                    self.store.send(.kanjiSelected(row))
-                                } label: {
-                                    ExpandedView(alignment: .center) {
-                                        VStack(spacing: 10) {
-                                            Text(data.kanjiInfo.kanji)
-                                                .foregroundStyle(Color.black)
-                                                .font(.system(size: 30, weight: .medium))
-                                            
-                                            if self.store.selectedKanjiRow == row {
-                                                if let detailInfo = self.store.selectedKanjiDetailInfo {
-                                                    Text(detailInfo.totalYomi)
-                                                        .foregroundStyle(Color.black.opacity(0.8))
-                                                        .font(.system(size: 17, weight: .regular))
-                                                        .padding(.bottom, 10)
-                                                }
-                                                
-                                                Text(data.kanjiInfo.description)
-                                                    .foregroundStyle(Color.gray)
-                                                    .font(.system(size: 15, weight: .light))
+        ScrollViewReader { proxy in
+            StandardNavigationScrollView(title: "JLPT \(self.store.jlptLevel)", backBtnConfigAction: {
+                self.store.send(.backBtnTapped)
+            }) {
+                LazyVStack(spacing: 20) {
+                    ForEach(Array(self.store.kanjiList.enumerated()), id: \.offset) { row, data in
+                        MainStyleView {
+                            Button {
+                                self.store.send(.kanjiSelected(row))
+                            } label: {
+                                ExpandedView(alignment: .center) {
+                                    VStack(spacing: 10) {
+                                        Text(data.kanjiInfo.kanji)
+                                            .foregroundStyle(Color.black)
+                                            .font(.system(size: 30, weight: .medium))
+                                        
+                                        if self.store.selectedKanjiRow == row {
+                                            if let detailInfo = self.store.selectedKanjiDetailInfo {
+                                                Text(detailInfo.totalYomi)
+                                                    .foregroundStyle(Color.black.opacity(0.8))
+                                                    .font(.system(size: 17, weight: .regular))
+                                                    .padding(.bottom, 10)
                                             }
+                                            
+                                            Text(data.kanjiInfo.description)
+                                                .foregroundStyle(Color.gray)
+                                                .font(.system(size: 15, weight: .light))
                                         }
                                     }
-                                    .padding(30)
-                                    .overlay {
-                                        VStack {
-                                            ExpandedView(alignment: .leading) {
-                                                Text("\(row + 1)번째")
-                                                    .foregroundStyle(Color.black.opacity(0.4))
-                                                    .font(.system(size: 15, weight: .light))
-                                            }
+                                }
+                                .padding(30)
+                                .overlay {
+                                    VStack {
+                                        ExpandedView(alignment: .leading) {
+                                            Text("\(row + 1)번째")
+                                                .foregroundStyle(Color.black.opacity(0.4))
+                                                .font(.system(size: 15, weight: .light))
+                                        }
+                                        
+                                        Spacer()
+                                        
+                                        HStack {
+                                            Image(systemName: data.isFavoriteWord ? "star.fill" : "star")
+                                                .resizable()
+                                                .frame(width: 17, height: 17)
+                                                .foregroundStyle(Color.black.opacity(0.4))
+                                                .onTapGesture {
+                                                    self.store.send(.starBtnTapped(row))
+                                                }
                                             
                                             Spacer()
                                             
-                                            HStack {
-                                                Image(systemName: data.isFavoriteWord ? "star.fill" : "star")
-                                                    .resizable()
-                                                    .frame(width: 17, height: 17)
-                                                    .foregroundStyle(Color.black.opacity(0.4))
-                                                    .onTapGesture {
-                                                        self.store.send(.starBtnTapped(row))
-                                                    }
-                                                
-                                                Spacer()
-                                                
-                                                Image(
-                                                    systemName: self.store.selectedKanjiRow == row ?  "chevron.up"  : "chevron.down"
-                                                )
-                                                .resizable()
-                                                .frame(width: 15, height: 8)
-                                                .foregroundStyle(Color.black.opacity(0.4))
-                                            }
+                                            Image(
+                                                systemName: self.store.selectedKanjiRow == row ?  "chevron.up"  : "chevron.down"
+                                            )
+                                            .resizable()
+                                            .frame(width: 15, height: 8)
+                                            .foregroundStyle(Color.black.opacity(0.4))
                                         }
                                     }
-                                    .padding(20)
                                 }
+                                .padding(20)
                             }
-                            .id(row)
                         }
+                        .id(row)
                     }
-                    .padding(.vertical, 20)
-                    .onAppear {
-                        if let row = self.store.selectedKanjiRow {
-                            proxy.scrollTo(row, anchor: .center)
-                        }
+                }
+                .padding(.vertical, 20)
+                .onAppear {
+                    if let row = self.store.selectedKanjiRow {
+                        proxy.scrollTo(row, anchor: .center)
                     }
                 }
             }
