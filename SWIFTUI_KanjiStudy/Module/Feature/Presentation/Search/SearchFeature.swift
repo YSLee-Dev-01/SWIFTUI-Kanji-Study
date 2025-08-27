22f58bbee3b2c17702b9521a62a6603927cef424
Yslee-dev-01 <ysleedev01@gmail.com>
2025-08-27 14:01:19 +0900

    Feat(Search): 기본 디자인 구성 및 결과 표출 기능 추가

--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/Search/SearchFeature.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/Search/SearchFeature.swift
@@ -10,19 +10,34 @@ import ComposableArchitecture
 
 @Reducer
 struct SearchFeature: Reducer {
+    @Dependency(\.kanjiManager) private var kanjiManager
     
     @ObservableState
     struct State: Equatable {
-        
+        var insertedText: String = ""
+        var searchResult: [String] = []
+        var searchLoading = false
     }
     
     enum Action: Equatable {
         case backBtnTapped
+        case textInserted(String)
+        case searchRequest
     }
     
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .textInserted(let text):
+                state.insertedText = text
+                state.searchLoading = true
+                return .send(.searchRequest).debounce(id: "SearchRequest", for: 0.2, scheduler: DispatchQueue.main)
+                
+            case .searchRequest:
+                state.searchResult = kanjiManager.findKanji(by: state.insertedText)
+                state.searchLoading = false
+                return .none
+                
             default: return .none
             }
         }
--- SWIFTUI_KanjiStudy/Module/Feature/Presentation/Search/SearchView.swift
+++ SWIFTUI_KanjiStudy/Module/Feature/Presentation/Search/SearchView.swift
@@ -16,11 +16,56 @@ struct SearchView: View {
     }
     
     var body: some View {
-        StandardNavigationScrollView(title: "검색", backBtnConfigAction: {
-            self.store.send(.backBtnTapped)
-        }) {
-            Text("SearchView")
+        VStack(spacing: 20) {
+            LargeNavigationBar(title: "검색"){
+                self.store.send(.backBtnTapped)
+            }
+            
+            MainStyleView {
+                ExpandedView(alignment: .leading) {
+                    TextField(text: self.$store.insertedText.sending(\.textInserted)) {
+                        Text("원하는 단어를 검색하세요.")
+                    }
+                }
+                .font(.system(size: 15, weight: .medium))
+                .foregroundColor(.black.opacity(0.7))
+                .padding(15)
+            }
+            .padding(.horizontal, 20)
+            
+            ScrollView {
+                if !self.store.insertedText.isEmpty && self.store.searchResult.isEmpty && !self.store.searchLoading {
+                    Text("검색 결과가 없어요.")
+                        .font(.system(size: 17, weight: .semibold))
+                        .foregroundColor(.black.opacity(0.7))
+                    
+                } else if self.store.searchLoading {
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                        .padding()
+                    
+                }  else {
+                    LazyVGrid(columns: [GridItem(.flexible(), spacing: 20), GridItem(.flexible(), spacing: 20)], spacing: 20) {
+                        ForEach(Array(self.store.searchResult.enumerated()), id: \.offset) { row, data in
+                            MainStyleView {
+                                Button {
+                                    
+                                } label: {
+                                    ExpandedView(alignment: .center) {
+                                        Text("\(data)")
+                                            .foregroundStyle(Color.black)
+                                            .font(.system(size: 22, weight: .medium))
+                                            .frame(height: 65)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 20)
+                }
+            }
         }
+        .padding(.vertical, 20)
     }
 }
 
